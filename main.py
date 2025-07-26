1c45c6e807892509c618b286fb5feaaa05bc181f
jawiker <ablauvelt93@gmail.com>
2025-07-26 11:22:22 -0400

    added sort_dict,sor_on_num to stats.py; updated main.pyreturn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import count_words
 from stats import count_chars
+from stats import sort_dict
 
 def get_book_text(book_file_path):
     with open(book_file_path) as book:
@@ -9,9 +10,18 @@ def get_book_text(book_file_path):
 def main():
     book = get_book_text("books/frankenstein.txt")
     count = count_words(book)
-    result = f"{count} words found in the document"
+    count_out = f"{count} words found in the document"
     char_count = count_chars(book)
-    print(result)
-    print(char_count)
+    sorted = sort_dict(char_count)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(count_out)
+    print("--------- Character Count -------")
+    for char in sorted:
+        if char["char"].isalpha():
+            print(f"{char["char"]}: {char["num"]}")
+    #print(sorted)
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,17 @@ def count_chars(book_string):
         else:
             chars[char] = 1
     return chars
+
+def sort_on_num(items):
+    return items["num"]
+
+def sort_dict(dict_object):
+    return_list = []
+    for obj in dict_object:
+        num = dict_object[obj]
+        new_dict = {"char":obj,"num":num}
+        return_list.append(new_dict)
+    
+    return_list.sort(reverse=True,key=sort_on_num)
+    return return_list
+
